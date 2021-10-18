3b076706a49f55cc64356a330dc144d2d2ee6c74
dat-adi <dat.adithya@gmail.com>
2021-10-18 17:52:35 +0530

    Abstracted the application from the server
    Created a separate application file for better modularization and
    cleaner code when routes are implemented.

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Set up testing routes
+app.get('/', function(req, res){
+    return res.json({
+        message: "This is supposed to be a GET request, I think?",
+        success: true
+    })
+});
+
+app.post('/', function(req, res){
+    return res.json({
+        message: "This is supposed to be a POST request, I think?",
+        success: true
+    })
+});
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get('/', function(req, res){
-    return res.json({
-        message: "This is supposed to be a GET request, I think?",
-        success: true
-    })
-});
-
-app.post('/', function(req, res){
-    return res.json({
-        message: "This is supposed to be a POST request, I think?",
-        success: true
-    })
-});
+const app = require("./app");
 
 app.listen(3000, () => console.log("The simple shopping app is listening on PORT 3000!"));
