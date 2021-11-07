bbc84d6f86caf22ca6ed31ba0d899fb43f5d8f95
dat-adi <dat.adithya@gmail.com>
2021-11-07 14:17:57 +0530

    Importing actual routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+// Importing the routes
+const medication = require("./routes/medication");
+
+app.use("/api/v1", medication);
+
 // Set up testing routes
 app.get('/', function(req, res){
     return res.json({
