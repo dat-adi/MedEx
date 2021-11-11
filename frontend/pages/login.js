2a41eef8f29d7e45a759db70e44b08cb210edc41
pavansantosh-ps <pavansantosh.ips@gmail.com>
2021-11-11 21:17:54 +0530

    Login submission

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,36 +1,60 @@
 import { useState } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:4000/"
 
 const login = () => {
 
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(email,password);
+        axios.post('/user/register',{
+            email: email,
+            password: password,
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+        
+    }
+
+    function handleEmailChange(e){
+        setEmail(e.target.value);
+    }
+
+    function handlePassChange(e){
+        setPassword(e.target.value);
+    }
 
 
     return (
         <div>
-            <div class="flex items-center justify-center h-screen">
-                <form class="block bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 block">
-                    <h1 class="block text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl pb-5">
-                        <span class="block text-indigo-600 xl:inline">MedEx</span> 
-                        <span class="block xl:inline"> Login</span>
+            <div className="flex items-center justify-center h-screen">
+                <form onSubmit = {handleSubmit} className="block bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 block">
+                    <h1 className="block text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl pb-5">
+                        <span className="block text-indigo-600 xl:inline">MedEx</span> 
+                        <span className="block xl:inline"> Login</span>
                     </h1>
-                    <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Email">
+                    <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="Email">
                         Email
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Email" type="text" placeholder="Email"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={handleEmailChange} id="Email" type="text" placeholder="Email"/>
                     </div>
-                    <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" onChange={handlePassChange} id="password" type="password" placeholder="Password"/>
                     </div>
-                    <div class="flex items-center justify-between">
-                    <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <div className="flex items-center justify-between">
+                    <button type="submit" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         Login
                     </button>
-                    
-                        Forgot Password?
                     </div>
                 </form>
             </div>
