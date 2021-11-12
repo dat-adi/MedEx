36867c2bf5b4e2e139274c5d61a42dd94a01ee1d
dat-adi <dat.adithya@gmail.com>
2021-11-12 17:41:25 +0530

    Added route permissions
    Only specified roles can perform CUD operations now.
    Error Handling for unauthorized, and login for user still needs to be
    added.

--- backend/routes/medication.js
+++ backend/routes/medication.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 // Importing the required controllers from the controllers folder
 const {getAllMedicines, createMedicine} = require("../controllers/medicationController");
 const {getMedicine, updateMedicine, deleteMedicine} = require("../controllers/medicationController");
 
 // Calling the respective controllers for the required operations
-router.route("/medication").get(isAuthenticatedUser, getAllMedicines);
-router.route("/medication").post(createMedicine);
-router.route("/medication/:medicationId").get(getMedicine).put(updateMedicine).delete(deleteMedicine);
+router.route("/medication").get(getAllMedicines);
+router.route("/medication")
+    .post(isAuthenticatedUser, authorizeRoles("admin"), createMedicine);
+router.route("/medication/:medicationId")
+    .get(isAuthenticatedUser, getMedicine)
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateMedicine)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteMedicine);
 
 // Exporting the router
 module.exports = router;
